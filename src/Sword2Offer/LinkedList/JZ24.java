0ab20f9344c92aa27167150ee4f942c9023507c2
xiaowenbin <xiaowenbin@ylmf1000.onexmail.com>
2022-02-25 18:45:00 +0800

    feat：study sword2offer

--- src/Sword2Offer/LinkedList/JZ24.java
+++ src/Sword2Offer/LinkedList/JZ24.java
@@ -1,4 +1,38 @@
 package Sword2Offer.LinkedList;
 
+import java.util.Stack;
+
 public class JZ24 {
+//    public static ListNode ReverseList(ListNode head) {
+//        if (head == null || head.next == null) {
+//             return head;
+//        }
+//        Stack<ListNode> temp = new Stack<>();
+//        while (head != null) {
+//            temp.push(head);
+//            head = head.next;
+//        }
+//        ListNode node = temp.pop();
+//        ListNode dummy = node;
+//        while (!temp.isEmpty()) {
+//            ListNode tempNode = temp.pop();
+//            node.next = tempNode;
+//            node = node.next;
+//        }
+//        node.next = null;
+//        return dummy;
+//    }
+    public static ListNode ReverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = temp;
+        }
+        return newHead;
+    }
 }
