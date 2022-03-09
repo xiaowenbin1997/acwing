49633454d72d4646c6aafd2a008b0b4b9cf08494
xiaowenbin <xiaowenbin@ylmf1000.onexmail.com>
2022-03-09 19:23:12 +0800

    feat:jz7

---
+++ src/Sword2Offer/Tree/JZ7Test.java
@@ -0,0 +1,20 @@
+package Sword2Offer.Tree;
+
+public class JZ7Test {
+    public static void main(String[] args) {
+        String preOrder = "1,2,4,7,3,5,6,8";
+        String inOrder = "4,7,2,1,5,3,8,6";
+        int[] pre = str2int(preOrder);
+        int[] vin = str2int(inOrder);
+
+    }
+
+    public static int[] str2int(String str) {
+        String[] strs = str.split(",");
+        int[] nums = new int[strs.length];
+        for (int i = 0; i < strs.length; i++) {
+            nums[i] = Integer.parseInt(strs[i]);
+        }
+        return nums;
+    }
+}
